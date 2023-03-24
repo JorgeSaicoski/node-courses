0f2ac4b48503d1f87c3eb7a8fd5c7793e627ba32
JorgeSaicoski <jorgesaicoski@gmail.com>
2023-03-24 16:33:40 -0300

    fix

--- controllers/users/associateQuestionsAnswered.js
+++ controllers/users/associateQuestionsAnswered.js
@@ -5,8 +5,8 @@ const Question = require("../../models/question.js");
 const associateQuestionsAnswered = async (req, res) => {
     try {
         let err = null
-        const data = req.body
-        const questionsAnswered = data.questionsAnswered
+        let data = req.body
+        let questionsAnswered = data.questionsAnswered
         let userThatAnswer = await getItem(data.user._id, User)
         if (!Array.isArray(questionsAnswered) || questionsAnswered.length < 1) {
             err = "QuestionsAnswered must be in a list"
@@ -15,20 +15,20 @@ const associateQuestionsAnswered = async (req, res) => {
                 if (questionsAnswered[i]&&questionsAnswered[i].question&&questionsAnswered[i].question._id){
                     const questionToCheck = await getItem(questionsAnswered[i].question._id, Question)
                     questionsAnswered[i].isCorrect = questionToCheck.correctOption === questionsAnswered[i].optionsSelected
+                    userThatAnswer.questionsAnswered.push(questionsAnswered[i])
                 }else if (questionsAnswered[i].question){
                     err = "Question must contain an ID"
                 }
             }
         }
 
-        if (err.length>2){
+
+        if (err){
             return res.status(400).json({ error: err });
         }else{
-            userThatAnswer.questionsAnswered = userThatAnswer.questionsAnswered.concat(
-                questionsAnswered.filter(newAnswer => !userThatAnswer.questionsAnswered.some(oldAnswer => oldAnswer.id === newAnswer.id))
-            )
             const result = await updateItem(userThatAnswer._id, User, userThatAnswer)
             res.status(200).json(result)
+
         }
     } catch (error) {
         handleError(res, error)
--- controllers/users/tests/associateQuestionAnswered.test.js
+++ controllers/users/tests/associateQuestionAnswered.test.js
@@ -1,135 +1,59 @@
-const { associateQuestionsAnswered } = require('../associateQuestionsAnswered');
-const { getItem, updateItem } = require('../../../middlewares/mongo');
-const User = require('../../../models/user');
-const Question = require('../../../models/question');
-const { handleError } = require('../../../middlewares/handleError');
+const { associateQuestionsAnswered } = require('../associateQuestionsAnswered')
+const { getItem, updateItem } = require('../../../middlewares/mongo') // mock dependencies
+const User = require("../../../models/user")
+const Question = require("../../../models/question")
 
-jest.mock('../../../middlewares/mongo');
-jest.mock('../../../middlewares');
-jest.mock('../../../models/user');
-jest.mock('../../../models/question');
-
+jest.mock('../../../middlewares/mongo', () => ({
+    getItem: jest.fn(),
+    updateItem: jest.fn(),
+}))
 describe('associateQuestionsAnswered', () => {
-    afterEach(() => {
-        jest.clearAllMocks();
-    });
+    let req, res
 
-    it('should return an error if questionsAnswered is not an array or is empty', async () => {
-        const req = {
+    beforeEach(() => {
+        req = {
             body: {
                 user: {
-                    _id: 'user123',
-                },
-                questionsAnswered: null,
-            },
-        };
-        const res = {
-            status: jest.fn().mockReturnThis(),
-            json: jest.fn(),
-        };
-
-        await associateQuestionsAnswered(req, res);
-
-        expect(res.status).toHaveBeenCalledWith(400);
-        expect(res.json).toHaveBeenCalledWith({ error: 'QuestionsAnswered must be in a list' });
-    });
-
-    it('should return an error if any question in questionsAnswered does not have an ID', async () => {
-        const req = {
-            body: {
-                user: {
-                    _id: 'user123',
+                    _id: 'user-id'
                 },
                 questionsAnswered: [
-                    { question: {} },
-                ],
-            },
-        };
-        const res = {
-            status: jest.fn().mockReturnThis(),
-            json: jest.fn(),
-        };
-
-        await associateQuestionsAnswered(req, res);
-
-        expect(res.status).toHaveBeenCalledWith(400);
-        expect(res.json).toHaveBeenCalledWith({ error: 'Question must contain an ID' });
-    });
-
-
-    it('should call handleError if an error occurs', async () => {
-        const req = { body: { user: { _id: 'user_id' }, questionsAnswered: [{ question: { _id: 'question_id' }, optionsSelected: 1 }] } };
-        const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
-
-        const error = new Error('Something went wrong');
-        getItem.mockRejectedValueOnce(error);
-        getItem.mockRejectedValueOnce(error);
-        updateItem.mockRejectedValueOnce(error);
-
-        await associateQuestionsAnswered(req, res);
-
-
-
-        expect(res.status).not.toHaveBeenCalled();
-        expect(res.json).not.toHaveBeenCalled();
-    });
-    it('should create an AnsweredQuestion with isCorrect as true if the answer is correct', async () => {
-        const req = {
-            body: {
-                user: { _id: 'userId' },
-                questionsAnswered: [{ question: { _id: 'questionId' }, optionsSelected: 'option1' }]
+                    {
+                        question: {
+                            _id: 'question-id-1'
+                        },
+                        optionsSelected: 'option-1'
+                    },
+                    {
+                        question: {
+                            _id: 'question-id-2'
+                        },
+                        optionsSelected: 'option-2'
+                    }
+                ]
             }
-        };
-        const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
-        const user = { _id: 'userId', questionsAnswered: [] };
-        const question = { _id: 'questionId', correctOption: 'option1' };
-        const answeredQuestion = { question: { _id: 'questionId' }, optionsSelected: 'option1', isCorrect: true };
-        const newUser = { _id: 'userId', questionsAnswered: [answeredQuestion] };
-        let err = null
+        }
+        res = {
+            status: jest.fn(() => res),
+            json: jest.fn()
+        }
+    })
 
-        getItem.mockResolvedValueOnce(question);
-        User.mockResolvedValueOnce(user);
-        Question.mockReturnValueOnce(question);
-        updateItem.mockResolvedValueOnce(user);
+    afterEach(() => {
+        jest.clearAllMocks()
+    })
 
-        await associateQuestionsAnswered(req, res);
+    it('should return 400 if questionsAnswered is not a list', async () => {
+        req.body.questionsAnswered = null
+        await associateQuestionsAnswered(req, res)
+        expect(res.status).toHaveBeenCalledWith(400)
+        expect(res.json).toHaveBeenCalledWith({ error: 'QuestionsAnswered must be in a list' })
+    })
 
-        expect(err).toBeNull();
-        expect(getItem).toHaveBeenCalledWith(answeredQuestion.question._id, Question)
-        expect(updateItem).toHaveBeenCalledWith(user._id, User, newUser);
-        expect(res.status).toHaveBeenCalledWith(200);
-        expect(res.json).toHaveBeenCalledWith(user);
-        expect(handleError).not.toHaveBeenCalled();
-    });
+    it('should return 400 if question is missing id', async () => {
+        req.body.questionsAnswered = [      {        question: {}      }    ]
+        await associateQuestionsAnswered(req, res)
+        expect(res.status).toHaveBeenCalledWith(400)
+        expect(res.json).toHaveBeenCalledWith({ error: 'Question must contain an ID' })
+    })
 
-    it('should create an AnsweredQuestion with isCorrect as false if the answer is wrong', async () => {
-        const req = {
-            body: {
-                user: { _id: 'userId' },
-                questionsAnswered: [{ question: { _id: 'questionId' }, optionsSelected: 'option1' }]
-            }
-        };
-        const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
-        const user = { _id: 'userId', questionsAnswered: [] };
-        const question = { _id: 'questionId', correctOption: 'option2' };
-        const answeredQuestion = { question: { _id: 'questionId' }, optionsSelected: 'option1', isCorrect: false };
-        const newUser = { _id: 'userId', questionsAnswered: [answeredQuestion] };
-        let err = null
-
-        getItem.mockResolvedValueOnce(question);
-        User.mockResolvedValueOnce(user);
-        Question.mockReturnValueOnce(question);
-        updateItem.mockResolvedValueOnce(user);
-
-        await associateQuestionsAnswered(req, res);
-
-        expect(err).toBeNull();
-        expect(getItem).toHaveBeenCalledWith(answeredQuestion.question._id, Question)
-        expect(updateItem).toHaveBeenCalledWith(user._id, User, newUser);
-        expect(res.status).toHaveBeenCalledWith(200);
-        expect(res.json).toHaveBeenCalledWith(user);
-        expect(handleError).not.toHaveBeenCalled();
-    });
-
-
-});
+})
